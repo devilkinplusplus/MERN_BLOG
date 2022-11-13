1c98b3dec79cd187b667cb4449a495262d4fdcd9
Ilkin Rufullayev <rufullayevilkin66@gmail.com>
2022-11-13 09:49:18 +0400

    some edits in server

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const http = require("http");
+const fs = require("fs");
+
+//listen files
+const mainPage = fs.readFileSync("main.html");
+const myPage = fs.readFileSync("ilkin.html");
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end("Hello, Node JS!");
+  if (req.url === "/") {
+    res.end(mainPage);
+  } 
+  else if (req.url === "/ilkin") {
+    res.end(myPage);
+  } 
+  else {
+    res.statusCode = 404;
+    res.end("Chupa Not Found");
+  }
 };
 
 const server = http.createServer(requestListener);
 //url : localhost:8080
-server.listen(8080);
+server.listen(8080, function () {
+  console.log("Server started on : http://localhost:8080");
+});
